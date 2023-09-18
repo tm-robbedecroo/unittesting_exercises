d4f990e88c31cc1cb9ee6323adaf8fec448ac540
Robbe Decroo <r0840260@student.thomasmore.be>
2023-09-18 09:09:34 +0200

    sum of 3 smallest ints

--- src/test/java/ex01_arrays/ArrayOfInts3SmallestIntsTest.java
+++ src/test/java/ex01_arrays/ArrayOfInts3SmallestIntsTest.java
@@ -7,9 +7,30 @@ import static org.junit.jupiter.api.Assertions.*;
 class ArrayOfInts3SmallestIntsTest {
 
     @Test
-    void sumOf3SmallestInts() {
+    void sumOf3SmallestInts_happyflow() {
         ArrayOfInts aoi = new ArrayOfInts();
         int i = aoi.sumOf3SmallestInts(new int[]{1,2,3,4});
         assertEquals(6, i);
+    }
+
+    @Test
+    void sumOf3SmallestInts_arrayNull() {
+        ArrayOfInts aoi = new ArrayOfInts();
+        int i = aoi.sumOf3SmallestInts(null);
+        assertEquals(0, i);
+    }
+
+    @Test
+    void sumOf3SmallestInts_arrayEmpty() {
+        ArrayOfInts aoi = new ArrayOfInts();
+        int i = aoi.sumOf3SmallestInts(new int[] {});
+        assertEquals(0, i);
+    }
+
+    @Test
+    void sumOf3SmallestInts_arraySmall() {
+        ArrayOfInts aoi = new ArrayOfInts();
+        int i = aoi.sumOf3SmallestInts(new int[] {1,2});
+        assertEquals(3, i);
     }
 }
